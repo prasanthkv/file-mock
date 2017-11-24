ba465b736c757b32f2420eca5f50b73185b09c08
kanchana-prasanth <kanchana.prasanth@gmail.com>
2017-11-23 18:13:17 -0800

    Mock Annotations

---
+++ file-mock/src/main/java/com/kvlabs/filemock/anotation/FileToMockAnnotations.java
@@ -0,0 +1,45 @@
+package com.kvlabs.filemock.anotation;
+
+import static com.kvlabs.filemock.FileToMock.mock;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+
+/**
+ * @author kanchana-prasanth
+ */
+public class FileToMockAnnotations {
+
+    /**
+     * Initializes objects annotated with FileMock annotations for given
+     * testClass: {@link com.kvlabs.filemock.anotation.MockFromFile}
+     *
+     * @param testClass
+     */
+    public static void initMocks(Object testClass) {
+        if (testClass == null) {
+            throw new IllegalArgumentException("testClass cannot be null. For info how to use @MockFromFile annotations see examples in javadoc for FileMockAnnotations class");
+        }
+        //all is well 
+        try {
+            Class<?> clazz = testClass.getClass();
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field field : fields) {
+                for (Annotation annotation : field.getAnnotations()) {
+                    //evaluate only mock
+                    if (MockFromFile.class == annotation.annotationType()) {
+                        MockFromFile mockAnotation = field.getAnnotation(MockFromFile.class);
+                        //Deserialize 
+                        Object fieldValue = mock(mockAnotation.path(), mockAnotation.mapper(), mockAnotation.safeMode(), field.getType());
+                        if (fieldValue != null) {
+                            //set accessible
+                            field.setAccessible(true);
+                            field.set(testClass, fieldValue);
+                        }
+                    }
+                }
+            }
+        } catch (IllegalAccessException | IllegalArgumentException | SecurityException ex) {
+            throw new RuntimeException("faild to access fileds");
+        }
+    }
+}
